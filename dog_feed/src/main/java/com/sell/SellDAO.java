501555916fb6e31b24efb1ceec0ea0a4ed4ba7fa
SoYoung <SoYoung@DESKTOP-RUVFPAO>
2022-05-02 20:51:28 +0900

    소영 - 상품(사료,간식)리스트, 상세정보

--- dog_feed/src/main/java/com/sell/SellDAO.java
+++ dog_feed/src/main/java/com/sell/SellDAO.java
@@ -12,15 +12,24 @@ import com.util.DBConn;
 public class SellDAO {
 	private Connection conn = DBConn.getConnection();
 	
-	public int dataCount() {
+	public int dataCount(String keyword) {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql;
 		
 		try {
-			sql = "SELECT NVL(COUNT(*), 0) FROM product";
+			sql = " SELECT NVL(COUNT(*), 0) "
+			    + "	FROM product p "
+				+ " JOIN CategoryDetail c ON p.category_Num = c.category_Num " ;
+
+			if(! keyword.equals("all")) {
+				sql += " WHERE categoryDetail_Name = ? ";
+			}
 			pstmt = conn.prepareStatement(sql);
+			if(! keyword.equals("all")) {
+				pstmt.setString(1, keyword);
+			}
 			
 			rs = pstmt.executeQuery();
 			
@@ -49,7 +58,8 @@ public class SellDAO {
 		return result;	
 	}
 	
-	public List<SellDTO> listProduct(int start, int end) {
+	// 상품 목록 가져오기
+	public List<SellDTO> listProduct(int start, int end, String keyword) {
 		List<SellDTO> list = new ArrayList<SellDTO>();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -67,15 +77,26 @@ public class SellDAO {
 			sb.append("                       ROW_NUMBER() OVER(PARTITION BY product_Num ORDER BY image_Num ASC) rank ");
 			sb.append("                 FROM ProductImage");
 			sb.append("             ) WHERE rank = 1 ");
-			sb.append("         ) i ON p.product_Num = i.product_Num ");			
+			sb.append("         ) i ON p.product_Num = i.product_Num ");
+			
+			if(! keyword.equals("all")) {
+				sb.append("     WHERE categoryDetail_Name = ? ");
+			}
+			
 			sb.append("			ORDER BY product_Hits DESC ");
 			sb.append("		) tb WHERE ROWNUM <= ? ");
 			sb.append(" ) WHERE rnum >= ? ");
 			
 			pstmt = conn.prepareStatement(sb.toString());
 			
-			pstmt.setInt(1, end);
-			pstmt.setInt(2, start);
+			if(keyword.equals("all")) {
+				pstmt.setInt(1, end);
+				pstmt.setInt(2, start);
+			} else {
+				pstmt.setString(1, keyword);
+				pstmt.setInt(2, end);
+				pstmt.setInt(3, start);
+			}
 			
 			rs = pstmt.executeQuery();
 			
@@ -114,6 +135,31 @@ public class SellDAO {
 		return list;	
 	}
 	
+	// 조회수 증가
+	public void updateHitCount(int product_Num) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "UPDATE product SET product_Hits = product_Hits+1 WHERE product_Num = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, product_Num);
+			
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e2) {
+				}
+			}
+		}
+	}
+	
+	// 상품 불러오기
 	public SellDTO readSell(int product_Num) {
 		SellDTO dto = null;
 		
@@ -122,7 +168,7 @@ public class SellDAO {
 		String sql;
 		
 		try {
-			sql = "SELECT p.product_Num, p.category_Num, product_Name, product_Price, categoryDetail_Name, categoryDetail_Kind, product_Hits "
+			sql = "SELECT p.product_Num, p.category_Num, product_Name, product_Price, product_Date, product_Info, categoryDetail_Name, categoryDetail_Kind, product_Hits "
 				+ " FROM product p "
 				+ " JOIN CategoryDetail c ON p.category_Num = c.category_Num "
 				+ " WHERE product_Num = ?";
@@ -140,6 +186,8 @@ public class SellDAO {
 				dto.setCategory_Num(rs.getInt("category_Num"));
 				dto.setProduct_Name(rs.getString("product_Name"));
 				dto.setProduct_Price(rs.getInt("product_Price"));
+				dto.setProduct_Date(rs.getString("product_Date"));
+				dto.setProduct_Info(rs.getString("product_Info"));
 				dto.setCategoryDetail_Name(rs.getString("categoryDetail_Name"));
 				dto.setCategoryDetail_Kind(rs.getString("categoryDetail_Kind"));
 				dto.setProduct_Hits(rs.getInt("product_Hits"));
@@ -166,6 +214,7 @@ public class SellDAO {
 		return dto;
 	}
 	
+	// 상품 이미지 불러오기
 	public List<SellDTO> listImage(int product_Num) {
 		List<SellDTO> list = new ArrayList<>();
 		PreparedStatement pstmt = null;
--- dog_feed/src/main/webapp/WEB-INF/views/sell/article.jsp
+++ dog_feed/src/main/webapp/WEB-INF/views/sell/article.jsp
@@ -14,18 +14,147 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="${pageContext.request.contextPath}/resource/css/styles.css" rel="stylesheet" type="text/css"/>
+<style type="text/css">
+h3 {
+	width:20%;
+	padding: 15px 10px;
+	margin-left: 300px;
+	border-bottom: 3px solid #424951;
+}
+
+main {
+	display: block;
+}
+
+a {
+    color: #222;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+a:hover {
+	color: #FFC19E;
+	 text-decoration: underline;
+}
+
+.table {
+	width:60%; margin: 0 auto; 
+	text-align: center;
+}
+
+.table tr:first-child {
+	border-top: 2px solid #212529; 
+}
+
+.table-form {
+	text-align: left;
+}
+
+.table-submit tr td {
+	padding: 10px 0;
+}
+
+.img-box {
+	max-width: 1000px;
+	padding: 5px;
+	box-sizing: border-box;
+	border: 1px solid #ccc;
+	display: flex; 
+	flex-direction: row; 
+	flex-wrap: nowrap;
+	overflow-x: auto;
+}
+
+.img-box img {
+	width: 50%;
+	margin-right: 5px;
+	flex: 0 0 auto;
+	cursor: pointer;
+}
+
+.btn {
+	color: #333;
+    border: 1px solid #999;
+    background-color: #fff;
+    padding: 5px 10px;
+    border-radius: 4px;
+    font-weight: 500;
+    cursor: pointer;
+    font-size: 14px;
+    font-family: "맑은 고딕", 나눔고딕, 돋움, sans-serif;
+    vertical-align: baseline;
+}
+
+.btn:active, .btn:focus, .btn:hover {
+	background-color: #f8f9fa;
+	color:#333;
+}
+
+
+</style>
+
 </head>
 
 <body>
 	<!-- navigation -->
 	<jsp:include page="/WEB-INF/views/layout/navigation.jsp"></jsp:include>
-   <!-- Header-->
+   	<!-- Header-->
 	<header>
 		<jsp:include page="/WEB-INF/views/layout/header.jsp"></jsp:include>
 	</header>
+	
+<main>
+	<h3>${dto.product_Name}</h3>
+		<p></p>
+		
+		<table class="table table-form">
+			
+				<tr>
+					<td colspan="2" style="border-bottom: none; padding-bottom: 0;">
+						카테고리 : ${dto.categoryDetail_Name}
+					</td>
+				</tr>
+				<tr>	
+					<td colspan="2">
+						카테고리 종류 : ${dto.categoryDetail_Kind}
+					</td>
+				</tr>	
+				<tr>
+					<td align="right">
+						조회 ${dto.product_Hits}
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" width="50%" style="border-bottom: none; padding-bottom: 0;">
+						가격 : ${dto.product_Price}원
+					</td>
+				</tr>
+				
+				<tr style="border-bottom: none;">
+					<td colspan="2" height="110">
+					${dto.product_Info}
+						<div class="img-box" style="border= none;">
+							<c:forEach var="vo" items="${listImage}">
+								<img src="${pageContext.request.contextPath}/uploads/management/${vo.image_Name}">
+							</c:forEach>
 
+						</div>
+					</td>	
+				</tr>
+			
+		</table>   
+		    
+		<table class="table table-submit">	
+				<tr>
+					<td align="right">
+						<button type="button" class="btn" onclick="location.href='${pageContext.request.contextPath}/sell/${bUri}.do';">리스트</button>
+					</td>
+				</tr>
+		</table>
+</main>
 
-
-메인
+<footer>
+    <jsp:include page="/WEB-INF/views/layout/footer.jsp"></jsp:include>
+</footer>
 </body>
 </html>
